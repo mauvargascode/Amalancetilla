eeb770d4981b90884f448a5747ccdc322226091b
mauvargascode <mauriciovargastm@gmail.com>
2023-02-06 22:51:25 -0600

    Carga

---
+++ Controllers/Login.php
@@ -0,0 +1,17 @@
+<?php 
+
+	class Login extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function login()
+		{
+			$data['page_tag'] = "Login - Amalancetilla";
+			$data['page_title'] = "Login";
+			$data['page_name'] = "login";
+			$data['page_functions_js'] = "functions_login.js";
+			$this->views->getView($this,"login",$data);
+		}
+    }
